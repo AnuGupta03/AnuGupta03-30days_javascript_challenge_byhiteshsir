f836144f500db4e0eb27788177e974d51e3a2dfa
Anuradha Gupta <anuradhagwl98@gmail.com>
2024-07-21 19:34:22 +0530

    Day_9_challenge completed

---
+++ Day_9_DOM_Manipulation/Index.js
@@ -0,0 +1,62 @@
+// Activity 1:
+// Task -1 
+    // document.getElementById('firstid').addEventListener('click', function(){
+    //     this.innerHTML = "Good morning";
+    // })
+// <!-- Task - 2
+    // var element = document.getElementsByClassName('secondclass')[0];
+    //     element.addEventListener('click', function(){
+    //         document.body.style.backgroundColor = "red";
+    //     })
+
+// <!-- Task - 3 -->
+
+    // var newvalue = document.createElement('div') 
+    // newvalue.innerText = "This is Third Task";
+    // document.body.appendChild(newvalue);
+
+// <!-- Task - 4 -->
+    // var newvalue = document.createElement('li')
+    // newvalue.innerText = "This is fourth Task"
+    // document.getElementById('mylist').appendChild(newvalue);
+    
+//  <!-- Task - 5 -->
+    // let removeelement = document.getElementById('fifthtask');
+    // removeelement.parentNode.removeChild(removeelement);
+
+// <!-- Task-6 -->
+
+            // var specificElements = document.getElementById('specificElement');
+            // if (specificElements.lastElementChild) {
+            //     specificElements.removeChild(specificElements.lastElementChild);
+            // }
+
+//  <!-- Task-7 -->
+
+    // let images = document.getElementById('image');
+    // images.setAttribute("src","https://cdn.pixabay.com/photo/2024/04/21/14/13/pelican-8710717_640.jpg")
+
+
+//   <!-- Task-8 -->
+// function addClass(){
+//     var element = document.getElementById("myElement");
+//     element.classList.add("highlight")
+// }
+
+
+// function removeClass(){
+//     var element = document.getElementById("myElement");
+//     element.classList.remove("highlights")
+// }
+
+// Task -9 
+// function changeTextContent(){
+//     var element = document.getElementById("myElement");
+//     element.innerHTML = "Good Evening"
+// }
+
+// Task-10
+    //    var element =  document.getElementById("myElement");
+    //    element.addEventListener('mouseover',function(){
+    //         element.style.borderColor = "blue";
+    //     });
